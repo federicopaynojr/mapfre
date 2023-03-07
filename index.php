beaad8aaab8a26b5d9827a9e34e9976d578023e1
Fed Payno <federico.payno@genesys.com>
2023-03-07 22:47:01 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<script type="text/javascript" charset="utf-8">
+  (function (g, e, n, es, ys) {
+    g['_genesysJs'] = e;
+    g[e] = g[e] || function () {
+      (g[e].q = g[e].q || []).push(arguments)
+    };
+    g[e].t = 1 * new Date();
+    g[e].c = es;
+    ys = document.createElement('script'); ys.async = 1; ys.src = n; ys.charset = 'utf-8'; document.head.appendChild(ys);
+  })(window, 'Genesys', 'https://apps.mypurecloud.com/genesys-bootstrap/genesys.min.js', {
+    environment: 'use1',
+    deploymentId: 'f232b87e-1e0f-454f-bc20-59e853baa31a'
+  });
+</script>
+
+
+<h1> Test</h1>
